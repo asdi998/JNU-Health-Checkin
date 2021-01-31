424138a0dc6a49085b39ae9d366bead4bb466016
asdi998 <asdi998@users.noreply.github.com>
2021-02-01 00:33:27 +0800

    Update checkin.py
    修复表格缺失的问题

--- checkin.py
+++ checkin.py
@@ -11,7 +11,6 @@ from requests import post
 
 class STUHealth():
     def __init__(self, username, password) -> None:
-        self.remove_list = ['personType', 'id', 'createTime', 'del']
         self.api = 'https://stuhealth.jnu.edu.cn/api/'
         self.headers = {
             'Connection': 'keep-alive',
@@ -40,41 +39,66 @@ class STUHealth():
 
     @staticmethod
     def encrypt(password) -> str:
+        # Init
         CRYPTOJSKEY = 'xAt9Ye&SouxCJziN'.encode('utf-8')
         BS = AES.block_size
-        def pad(s): return s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+        def _pad(s): return s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+        # Hash
         cipher = AES.new(CRYPTOJSKEY, AES.MODE_CBC, CRYPTOJSKEY)
-        enrypted = b64encode(cipher.encrypt(
-            pad(password).encode('utf-8'))).decode('utf-8')
+        enrypted = cipher.encrypt(_pad(password).encode('utf-8'))
+        enrypted = b64encode(enrypted).decode('utf-8')
         enrypted = enrypted.replace('/', '_').replace('+', '-').replace('=', '*', 1)
         return enrypted
 
-    def filter_table(self, table) -> dict:
-        new_table = {k: v for k, v in table.items() if v}
-        for i in self.remove_list:
-            del new_table[i]
-        if 'way2Start' not in new_table:
-            new_table['way2Start'] = ""
-        new_table['declareTime'] = datetime.now().strftime("%Y-%m-%d")
-        return new_table
+    @staticmethod
+    def filter_tables(mainTable, secondTable) -> tuple:
+        # Init
+        REMOVE_LIST = ['personType', 'id', 'createTime', 'del', 'mainId']
+        MAIN_ADD_LIST = ['way2Start']
+        MAIN_TO_SECOND = {
+            'inChina': 'other1',
+            'countryArea': 'other2',
+            'personC4': 'other3',
+            'personC1': 'other4',
+            'personC1id': 'other5',
+            'personC2': 'other6',
+            'personC2id': 'other7',
+            'personC3': 'other8',
+            'personC3id': 'other9',
+        }
+        # Filter Main Table
+        new_mainTable = {k: v for k, v in mainTable.items() if v if k not in REMOVE_LIST}
+        for i in MAIN_ADD_LIST:
+            if i not in new_mainTable:
+                new_mainTable[i] = ""
+        new_mainTable['declareTime'] = datetime.now().strftime("%Y-%m-%d")
+        # Filter Second Table
+        if secondTable is not None:
+            new_secondTable = {k: v for k, v in secondTable.items() if v if k not in REMOVE_LIST}
+        elif mainTable['currentArea'] == "1":
+            new_secondTable = {v: mainTable[k] for k, v in MAIN_TO_SECOND.items() if mainTable[k]}
+        else:
+            new_secondTable = secondTable
+        return new_mainTable, new_secondTable
 
     def login(self, username, password) -> None:
         if len(password) != 24:
-            password = STUHealth.encrypt(password)
+            password = self.encrypt(password)
         api = 'user/login'
         data = {"username": username, "password": password}
         self.login_msg = self.query(api, data)
         self.idtype = self.result['data']['idtype']
         self.jnuid = self.result['data']['jnuid']
         self.jnuId = self.result['data']['jnuId']
-        self.wrote = (self.result['meta']['code'] != 200)
+        self.need_write = (self.result['meta']['code'] == 200)
 
-    def stuinfo(self) -> dict:
+    def stuinfo(self) -> tuple:
         api = 'user/stuinfo'
         data = {"jnuid": self.jnuid, "idType": self.idtype}
         self.query(api, data)
         mainTable = self.result['data']['mainTable']
-        return mainTable
+        secondTable = self.result['data']['secondTable']
+        return mainTable, secondTable
 
     def stucheckin(self) -> int:
         api = 'user/stucheckin'
@@ -88,17 +112,21 @@ class STUHealth():
                 break
         return last_id
 
-    def review(self, id) -> dict:
+    def review(self, id) -> tuple:
         api = 'user/review'
         data = {"jnuid": self.jnuid, "id": id}
         self.query(api, data)
         mainTable = self.result['data']['mainTable']
-        return mainTable
+        secondTable = self.result['data']['secondTable']
+        return mainTable, secondTable
 
-    def write(self, mainTable) -> str:
+    def write(self, mainTable, secondTable) -> str:
         api = 'write/main'
         data = {"jnuid": self.jnuid, "mainTable": mainTable}
-        return self.query(api, data)
+        if secondTable is not None:
+            data['secondTable'] = secondTable
+        msg = self.query(api, data)
+        return msg
 
 
 def sc_send(text, desp='') -> None:
@@ -109,12 +137,12 @@ def sc_send(text, desp='') -> None:
 def checkin(username, password) -> Optional[str]:
     try:
         stu = STUHealth(username, password)
-        if stu.wrote:
+        if not stu.need_write:
             return
         last_id = stu.stucheckin()
-        last_table = stu.review(last_id)
-        mainTable = stu.filter_table(last_table)
-        msg = stu.write(mainTable)
+        last_tables = stu.review(last_id)
+        tables = stu.filter_tables(*last_tables)
+        msg = stu.write(*tables)
         return "打卡{}：{}".format(username, msg)
     except Exception as e:
         print('发生异常{}，详情如下：{}'.format(e, format_exc()))
